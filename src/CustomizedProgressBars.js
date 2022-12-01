c4875a7bcd014a9b118286378dcc5fe31bdcd05e
11m245 <sivaraj2siva@gmail.com>
2022-12-01 12:59:34 +0530

    smallcards

---
+++ src/CustomizedProgressBars.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme, barColor }) => ({
+    height: 7,
+    borderRadius: 5,
+    [`&.${linearProgressClasses.colorPrimary}`]: {
+        backgroundColor: "hsl(231deg 26 % 89 %)"
+    },
+    [`& .${linearProgressClasses.bar}`]: {
+        borderRadius: 5,
+        backgroundColor: barColor,
+    },
+}));
+
+
+
+export default function CustomizedProgressBars({ barColor, value }) {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <BorderLinearProgress barColor={barColor} variant="determinate" value={value} />
+        </Box>
+    );
+}
---
+++ src/SmallCards.js
@@ -0,0 +1,36 @@
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import MarkChatUnreadIcon from '@mui/icons-material/MarkChatUnread';
+import CustomizedProgressBars from './CustomizedProgressBars';
+
+export function SmallCards() {
+
+    const cardDataList = [{ label: "EARNINGS (MONTHLY)", labelColor: `var(--clr1)`, number: 40000, type: "money", progressbar: false, icon: <CalendarMonthIcon sx={{ fontSize: 40, color: `var(--dull-clr)` }} /> },
+    { label: "EARNINGS (ANNUAL)", labelColor: `var(--clr2)`, number: 215000, type: "money", progressbar: false, icon: <AttachMoneyIcon sx={{ fontSize: 40, color: `var(--dull-clr)` }} /> },
+    { label: "TASKS", labelColor: `var(--clr3)`, number: 50, type: "percent", progressbar: true, icon: <AssignmentIcon sx={{ fontSize: 40, color: `var(--dull-clr)` }} /> },
+    { label: "PENDING REQUESTS", labelColor: `var(--clr4)`, number: 18, type: "count", progressbar: false, icon: <MarkChatUnreadIcon sx={{ fontSize: 40, color: `var(--dull-clr)` }} /> }
+    ];
+
+    return (<div className='small-cards-container'>
+        {cardDataList.map((obj) => <SmallCard data={obj} />)}
+    </div>);
+
+}
+function SmallCard({ data }) {
+    return (<div style={{ borderLeft: `5px solid ${data.labelColor}` }} className="small-card-container">
+        <div>
+            <p style={{ color: data.labelColor }} className='small-card-label'> {data.label}</p>
+            <div className="number-container">
+                <h3 style={{ color: `var(--text-b-clr)` }} className='small-card-number'>
+                    {data.type === "money" ? `$${data.number}` : null}
+                    {data.type === "count" ? `${data.number}` : null}
+                    {data.type === "percent" ? `${data.number}% ` : null}
+                </h3>
+                {data.type === "percent" ? <CustomizedProgressBars barColor={data.labelColor} value={data.number} /> : null}
+            </div>
+        </div>
+        {data.icon}
+        {/* <CalendarMonthIcon sx={{ fontSize: 40, color: `var(--dull-clr)` }} /> */}
+    </div>);
+}
